3fa3efe87be8837f5d4e5e2d9365de59c9ef2472
Kargin Nikita <hei@spark-mail.ru>
2016-10-25 02:00:19 +0300

    GameCircules is now abstracted from other logic

--- src/lessons/lesson1/GameCircles.java
+++ src/lessons/lesson1/GameCircles.java
@@ -2,9 +2,6 @@ package lessons.lesson1;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.ArrayList;
 
 public class GameCircles extends JFrame {
 
@@ -12,82 +9,14 @@ public class GameCircles extends JFrame {
         new GameCircles();
     }
 
-    private GameCanvas gameCanvas;
-    private final ArrayList<Ball> balls = new ArrayList<>();
-
     private GameCircles(){
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocation(600, 200);
         setSize(800, 600);
         setResizable(false);
         setTitle("Кружки");
-        gameCanvas = new GameCanvas(this);
-        add(gameCanvas, BorderLayout.CENTER);
+        add(new GameCanvas(), BorderLayout.CENTER);
         setVisible(true);
     }
 
-    void onRepaint(Graphics g, float deltaTime){
-        update(deltaTime);
-        render(g);
-    }
-
-    private void update(float deltaTime){
-        for (int i = 0; i < balls.size(); i++) {
-            balls.get(i).update(deltaTime);
-        }
-    }
-
-    private void render(Graphics g){
-        for (int i = 0; i < balls.size(); i++) {
-            balls.get(i).render(g);
-        }
-    }
-
-    private class CustomListener implements MouseListener {
-
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            GameCircles.this.balls.add(new Ball(gameCanvas));
-        }
-
-        /**
-         * Invoked when a mouse button has been pressed on a component.
-         *
-         * @param e
-         */
-        @Override
-        public void mousePressed(MouseEvent e) {
-
-        }
-
-        /**
-         * Invoked when a mouse button has been released on a component.
-         *
-         * @param e
-         */
-        @Override
-        public void mouseReleased(MouseEvent e) {
-
-        }
-
-        /**
-         * Invoked when the mouse enters a component.
-         *
-         * @param e
-         */
-        @Override
-        public void mouseEntered(MouseEvent e) {
-
-        }
-
-        /**
-         * Invoked when the mouse exits a component.
-         *
-         * @param e
-         */
-        @Override
-        public void mouseExited(MouseEvent e) {
-
-        }
-    }
 }
--- src/lessons/lesson1/Sprite.java
+++ src/lessons/lesson1/Sprite.java
@@ -4,37 +4,40 @@ import java.awt.*;
 
 abstract class Sprite {
 
-    protected GameCanvas gameCanvas;
-    protected float x;
-    protected float y;
-    protected float halfWidth;
-    protected float halfHeight;
-
-    public Sprite(GameCanvas gameCanvas){
-        this.gameCanvas = gameCanvas;
-    }
-
-    public float getLeft(){ return x - halfWidth; }
-
-    public void setLeft(float left){ x = left + halfWidth; }
-
-    public float getRight(){ return x + halfWidth; }
-
-    public void setRight(float right){ x = right - halfWidth;}
-
-    public float getTop(){ return y - halfHeight; }
-
-    public void setTop(float top){ y = top + halfHeight; }
-
-    public float getBottom(){ return y + halfHeight; }
-
-    public void setBottom(float bottom){ y = bottom - halfHeight; }
-
-    public float getWidth(){ return 2f * halfWidth; }
-
-    public float getHeight(){return 2f * halfHeight; }
+    float x;
+    float y;
+    float halfWidth;
+    float halfHeight;
+    float vy;
+    float vx;
 
     abstract public void update(float deltaTime);
 
     abstract public void render(Graphics g);
+
+    float getLeft(){ return x - halfWidth; }
+
+    void setLeft(float left){ x = left + halfWidth; }
+
+    float getRight(){ return x + halfWidth; }
+
+    void setRight(float right){ x = right - halfWidth;}
+
+    float getTop(){ return y - halfHeight; }
+
+    void setTop(float top){ y = top + halfHeight; }
+
+    float getBottom(){ return y + halfHeight; }
+
+    void setBottom(float bottom){ y = bottom - halfHeight; }
+
+    float getWidth(){ return 2f * halfWidth; }
+
+    float getHeight(){return 2f * halfHeight; }
+
+    void revertVx() { this.vx = -this.vx; }
+
+    void revertVy() { this.vy = -this.vy; }
+
+
 }
